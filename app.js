4ec07c69ec062dcab26730af79a42632b8b86692
aydellast <ayudellaastuti23@gmail.com>
2026-04-09 18:05:45 +0700

    setup koneksi database MySQL

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-// Import express
 const express = require("express");
-// Membuat object express
 const app = express();
 const port = 3000;
 
-// ==========================================
-// ROUTING UTAMA & TRANSAKSI CUPPYCASH
-// ==========================================
+// Import file koneksi database (Tambahan Sprint 4)
+const db = require('./config/database'); 
 
-// Endpoint utama (Home)
-app.get("/", (req, res) => {
-    res.send("Welcome to CuppyCash API - Smart Finance Tracker!");
-});
+// Import file routes yang sudah kita buat
+const apiRouter = require('./routes/api'); 
+const transactionRoutes = require('./routes/transactions');
+const userRoutes = require('./routes/users');
 
-// Menampilkan semua data transaksi (Read)
-app.get("/transactions", (req, res) => {
-    res.send("Menampilkan semua data transaksi CuppyCash");
-});
-
-// Menambahkan data transaksi baru (Create)
-app.post("/transactions", (req, res) => {
-    res.send("Menambahkan data transaksi baru");
-});
-
-// Mengedit data transaksi (Update)
-app.put("/transactions", (req, res) => {
-    res.send("Mengedit data transaksi CuppyCash");
-});
-
-// Menghapus data transaksi (Delete)
-app.delete("/transactions", (req, res) => {
-    res.send("Menghapus data transaksi CuppyCash");
-});
+// Daftarkan rutenya di sini
+app.use('/', apiRouter); 
+app.use('/transactions', transactionRoutes); 
+app.use('/users', userRoutes); 
 
 // ==========================================
+// ENDPOINT KHUSUS TESTING DATABASE (Tugas Sprint 4)
+// ==========================================
+app.get("/test-db", (req, res) => {
+    db.query("SELECT 1 + 1 AS solution", (err, result) => {
+        if (err) {
+            res.json({ 
+                message: "Koneksi database gagal ❌", 
+                error: err 
+            });
+        } else {
+            res.json({ 
+                message: "Koneksi database berhasil 100%! ✅", 
+                result: result 
+            });
+        }
+    });
+});
+// ==========================================
 
-// Menjalankan server
 app.listen(port, () => {
     console.log(`CuppyCash Server is running on http://localhost:${port}`);
 });
---
+++ controllers/usersController.js
@@ -0,0 +1,5 @@
+//otak untuk mengatur tabel users (seperti login/register nanti)
+
+exports.getAllUsers = (req, res) => {
+    res.json({ message: "Menampilkan semua data pengguna CuppyCash" });
+};
