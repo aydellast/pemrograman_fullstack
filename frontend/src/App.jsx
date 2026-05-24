1e4bdb40e2bfd362fb4be11086b52c07af356151
Cinta Melati <cntmlt130@gmail.com>
2026-05-24 16:15:09 +0700

    Up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,33 +3,71 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
-<<<<<<< HEAD
-import Expenses from './pages/Expenses';
-=======
+
+import Expenses from "./pages/Expenses";
 import Category from "./pages/Category/Category";
 import Income from "./pages/Income/Income";
->>>>>>> 88fd08a (Selesai sprint 9)
+
+import Profile from "./pages/Profile/Profile";
+import Budget from "./pages/Budget/Budget";
 
 function App() {
+
   return (
     <BrowserRouter>
+
       <Routes>
 
-        <Route path="/" element={<Dashboard />} />
+        {/* DASHBOARD */}
+        <Route
+          path="/"
+          element={<Dashboard />}
+        />
 
-        <Route path="/login" element={<Login />} />
+        {/* LOGIN */}
+        <Route
+          path="/login"
+          element={<Login />}
+        />
 
-        <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
-        <Route path="/expenses" element={<Expenses />} />
-=======
+        {/* REGISTER */}
+        <Route
+          path="/register"
+          element={<Register />}
+        />
 
-        <Route path="/categories" element={<Category />} />
+        {/* EXPENSES */}
+        <Route
+          path="/expenses"
+          element={<Expenses />}
+        />
 
-        <Route path="/income" element={<Income />} />
+        {/* CATEGORY */}
+        <Route
+          path="/categories"
+          element={<Category />}
+        />
 
->>>>>>> 88fd08a (Selesai sprint 9)
+        {/* INCOME */}
+        <Route
+          path="/income"
+          element={<Income />}
+        />
+
+        {/* PROFILE */}
+        <Route
+          path="/profile"
+          element={<Profile />}
+        />
+
+        {/* BUDGET */}
+        <Route
+          path="/budget"
+          element={<Budget />}
+        />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,43 +1,57 @@
-<<<<<<< HEAD
 import styles from "./Navbar.module.css";
-=======
-import "./Navbar.css";
+
 import { Link } from "react-router-dom";
->>>>>>> 88fd08a (Selesai sprint 9)
 
 function Navbar() {
+
   return (
+
     <nav className={styles.navbar}>
+
       <h2>CuppyCash</h2>
 
-<<<<<<< HEAD
       <ul className={styles["nav-links"]}>
-        <li>Dashboard</li>
-        <li>Income</li>
-        <li>Expense</li>
-        <li>Profile</li>
-=======
-      <ul className="nav-links">
 
         <li>
-          <Link to="/">Dashboard</Link>
+          <Link to="/">
+            Dashboard
+          </Link>
         </li>
 
         <li>
-          <Link to="/income">Income</Link>
+          <Link to="/income">
+            Income
+          </Link>
         </li>
 
         <li>
-          <Link to="/categories">Category</Link>
+          <Link to="/expenses">
+            Expense
+          </Link>
         </li>
 
         <li>
-          <Link to="/profile">Profile</Link>
+          <Link to="/categories">
+            Category
+          </Link>
         </li>
 
->>>>>>> 88fd08a (Selesai sprint 9)
+        <li>
+          <Link to="/profile">
+            Profile
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/budget">
+            Budget
+          </Link>
+        </li>
+
       </ul>
+
     </nav>
+
   );
 }
 
