dac5c031463c7a5e511bef7ad3cd29ffdb3fe5c4
aydellast <ayudellaastuti23@gmail.com>
2026-06-04 04:40:24 +0700

    Sprint 10 login register API integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Expenses from './pages/Expenses';
+import ChartsPage from "./pages/Charts/ChartsPage";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/expenses" element={<Expenses />} />
+        <Route path="/charts" element={<ChartsPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,18 +1,71 @@
+import { useState } from "react";
+import api from "../../services/api";
 import styles from "./Register.module.css";
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert("Register berhasil!");
+      console.log(response.data);
+
+      setName("");
+      setEmail("");
+      setPassword("");
+
+    } catch (error) {
+      console.error(error);
+
+      alert(
+        error.response?.data?.message ||
+        "Register gagal"
+      );
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit}
+      >
         <h2>Register</h2>
 
-        <input type="text" placeholder="Full Name" />
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <input type="email" placeholder="Email" />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <input type="password" placeholder="Password" />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button type="submit">Register</button>
+        <button type="submit">
+          Register
+        </button>
       </form>
     </div>
   );
